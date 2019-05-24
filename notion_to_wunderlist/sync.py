3fe377913e326fac682b0c86879e4338098a0da0
patrickjmcd <patrickjmcd@gmail.com>
2019-05-24 13:13:45 -0500

    append all tasks with (notion)

--- notion_to_wunderlist/sync.py
+++ notion_to_wunderlist/sync.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from sys import argv
 
-from . import wunderlist_tasks
-from . import notion_tasks
+# from . import wunderlist_tasks
+# from . import notion_tasks
+
+import wunderlist_tasks
+import notion_tasks
 
 load_dotenv()
 
@@ -18,8 +21,8 @@ def status_is_same(notion_status, wunderlist_complete):
     return False
 
 
-def check_task_status(notion_task, wunderlist_task):
-    if notion_task.name == wunderlist_task['title']:
+def check_task_status(notion_task, w_task_name, wunderlist_task):
+    if w_task_name == wunderlist_task['title']:
         if not status_is_same(notion_task.status, wunderlist_task['completed']):
             if not wunderlist_task['completed']:
                 return 'update_wunderlist'
@@ -42,15 +45,16 @@ def main():
     for n_task in notion_tasklist:
         operation = 'create'
         update_w_task = {}
+        w_task_name = "{} (notion)".format(n_task.name)
         for w_task in wunderlist_tasklist:
-            task_operation = check_task_status(n_task, w_task)
+            task_operation = check_task_status(n_task, w_task_name, w_task)
             if task_operation:
                 operation = task_operation
                 update_w_task = w_task
 
         if operation == 'create':
             wunderlist_tasks.create_task(
-                wunderlist_list['id'], n_task.name, n_task.status == "Completed")
+                wunderlist_list['id'], w_task_name, n_task.status == "Completed")
         elif operation == 'update_wunderlist':
             wunderlist_tasks.update_task(
                 list_name, update_w_task, n_task.status == "Completed")
